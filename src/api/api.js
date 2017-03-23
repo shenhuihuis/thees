8f91624746076c4671f2ae54bb8b1ab7e2f96118
shenhuihuis <beiyou526604364@163.com>
2017-03-23 15:22:03 +0800

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,29 @@
-var api={
-    ip:[],
-    realm:[],//域名
-    modular:{
-        
+define(function(require){
+    var api={
+        ip: ["http://app.33etong.com/",       //省市区 获取验证码
+             "http://goods.33etong.com/",    //商品相关 
+             "http://user.33etong.com/",     //用户相关 
+             "http://order.33etong.com"     //订单相关  
+        ],
+        person:null,
+        Value: (function() {
+            var argName, args, pair, pairs, pos, query, value, _0, _1;
+            args = {};
+            query = window.location.search.substring(1);
+            pairs = query.split('&');
+            for (_0 = 0, _1 = pairs.length; _0 < _1; _0++) {
+                pair = pairs[_0];
+                pos = pair.indexOf('=');
+                if (pos === -1) {
+                    continue
+                }
+                argName = pair.substr(0, pos);
+                value = pair.substr(pos + 1);
+                value = decodeURIComponent(value);
+                args[argName] = value
+            }
+            return args
+        })()
     }
-}
+    return api;
+})
--- src/component/head.js
+++ src/component/head.js
@@ -1,12 +1,12 @@
 define(function(require){
     var Vue = require("vue");
-    var Public=require("public");
+    var api=require("api");
     var header = Vue.extend({
         template : '<div v-if="login">已登录</div><div v-else>未登录</div>',
         data: function () {
             return {
                 data: 1,
-                login:Public.token
+                login:api.person
             }
         }
     });
